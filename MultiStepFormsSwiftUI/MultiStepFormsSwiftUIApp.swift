223afcdb04f7e0e71b3ce6a1349cc6903e0e3338
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2025-03-24 14:58:25 +0000

    Initial project setup

---
+++ MultiStepFormsSwiftUI/MultiStepFormsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiStepFormsSwiftUIApp.swift
+//  MultiStepFormsSwiftUI
+//
+//  Created by Claire Roughan on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiStepFormsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
